5f52122e82a1495f912710b2e07d9003a445e978
Stephanot <stephanotkr@gmail.com>
2022-05-14 00:43:28 +0300

    document title

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,30 +1,46 @@
 import React, { Component } from 'react';
 import '../style/counter.scss';
 
+const getStateFromLocalStorage = () => {
+    const storage = localStorage.getItem('counterState');
+    if (storage) return JSON.parse(storage);
+    return {count: 0}
+};
+
+const storeStateInLocalStorage = (state: any) => {
+    localStorage.setItem('counterState', JSON.stringify(state))
+    console.log(localStorage)
+}
+
 class Counter extends Component <any, any> {
     constructor(props: any)
     {
         super(props);
-        this.state = {
-            count: 0,
-        }
+        this.state = getStateFromLocalStorage()
         this.increment = this.increment.bind(this)
         this.decrement = this.decrement.bind(this)
         this.reset = this.reset.bind(this)
+        this.updateDocumentTitle = this.updateDocumentTitle.bind(this)
+    }
+
+    updateDocumentTitle()
+    {
+        document.title = this.state.count
     }
 
     increment(){
-        const { max, step } = this.props;
-        this.setState((state: { count: number; }) => {
+        this.setState((state: { count: number, title: string }, props: {max: number, step: number}) => {
+            const { max, step } = props;
             if (state.count >= max) return;
-            return { count: state.count + step }
-        })
+            const incrementValue = state.count + step
+            return { count: incrementValue}
+        }, this.updateDocumentTitle )
     }
     decrement(){
-        this.setState({count: this.state.count - 1})
+        this.setState({count: this.state.count - 1}, this.updateDocumentTitle)
     }
     reset(){
-        this.setState({count: 0})
+        this.setState({count: 0}, this.updateDocumentTitle)
     }
 
     render() {
